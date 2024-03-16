57017240ec5f05416dcb8cbbc8f75aa1e0117cae
Konstantin Läufer <laufer@cs.luc.edu>
2024-03-16 14:37:14 -0500

    simplified tree building, fixes #4
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- src/bin/processtree.rs
+++ src/bin/processtree.rs
@@ -1,16 +1,20 @@
 use std::collections::HashMap;
-use std::num::ParseIntError;
 use std::time::SystemTime;
 use std::io::{self, BufRead};
+
+// for simplified error handling
+extern crate anyhow;
+use anyhow::Result;
 
 type Process = (usize, usize, String);
 
 type ProcessTree = HashMap<usize, Vec<Process>>;
 
-fn parse_header(header: &str) -> Option<impl Fn(&str) -> Result<Process, ParseIntError>> {
-    let cols: Vec<&str> = header.trim().split_whitespace().collect();
-    let i_pid = cols.iter().position(|&r| r == "PID")?;
-    let i_ppid = cols.iter().position(|&r| r == "PPID")?;
+/// Creates a line parser from a header of the form "... PID ... PPID ... COMMAND ..." (or CMD).
+fn make_parser_from_header(header: &str) -> Option<impl Fn(&str) -> Result<Process>> {
+    let cols: Vec<&str> = header.split_whitespace().collect();
+    let pos_in_cols = |col: &str| cols.iter().position(|&r| r == col).unwrap();
+    let [i_pid, i_ppid] = ["PID", "PPID"].map(pos_in_cols);
     let i_cmd = ["CMD", "COMMAND"].iter()
         .filter_map(|&cmd| header.find(cmd))
         .max()?;
@@ -23,14 +27,16 @@ fn parse_header(header: &str) -> Option<impl Fn(&str) -> Result<Process, ParseIn
     })
 }
 
+/// Builds a process tree from a flat list of processes.
 fn build_tree(processes: impl Iterator<Item = Process>) -> ProcessTree {
-    processes.fold(HashMap::new(), |mut tree, process| {
-        let (pid, ppid, cmd) = process;
+    let mut tree: HashMap<usize, Vec<(usize, usize, String)>> = HashMap::new();
+    for (pid, ppid, cmd) in processes {
         tree.entry(ppid).or_default().push((pid, ppid, cmd));
-        tree
-    })  
+    }
+    tree
 }
 
+/// Prints a process tree with indentation.
 fn print_tree(tree: &ProcessTree, pid: usize, depth: usize) {
     if let Some(processes) = tree.get(&pid) {
         for (pid, _, cmd) in processes {
@@ -40,14 +46,15 @@ fn print_tree(tree: &ProcessTree, pid: usize, depth: usize) {
     }
 }
 
-fn main() {
+fn main() -> Result<()> {
     let mut lines = io::stdin().lock().lines();
-    let header = lines.next().unwrap().unwrap();
-    let parser = parse_header(header.as_str()).unwrap();
-    let processes = lines.map(|line| line.unwrap()).map(|line| parser(&line).unwrap());
+    let header = lines.next().unwrap()?;
+    let parser = make_parser_from_header(header.as_str()).unwrap();
+    let processes = lines.map(|line: Result<String, io::Error>| line.unwrap()).map(|line| parser(&line).unwrap());
     let start = SystemTime::now();
     let tree = build_tree(processes);
-    let total = SystemTime::now().duration_since(start).unwrap();
+    let total = SystemTime::now().duration_since(start)?;
     print_tree(&tree, 0, 0);
     eprintln!("Processing time: {:?}", total);
+    Ok(())
 }
